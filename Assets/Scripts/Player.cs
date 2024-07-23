49170ebf813b9fee2909c8ab4792bf9084fdee9f
Alejandro Rodriguez <96800886+alejandrodriguez@users.noreply.github.com>
2024-07-23 19:28:33 -0400

    Added collision detection to Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,8 +14,35 @@ public class Player : MonoBehaviour
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = 0.7f;
+        int playerHeight = 2;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+        if (!canMove) {
+            // Attempt only X movement
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+            if (canMove)
+            {
+                // Can move only in the X direction
+                moveDir = moveDirX;
+            }
+            else
+            {
+                // Attempt only Z movement
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                if (canMove)
+                {
+                    moveDir = moveDirZ;
+                }
+            }
 
+        }
+        if (canMove)
+        { 
+            transform.position += moveDir * moveDistance;
+        }
         isWalking = moveDir != Vector3.zero;
         int rotationSpeed = 10;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, rotationSpeed * Time.deltaTime);
