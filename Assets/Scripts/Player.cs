b4e5f42182af5d062864dcecde27396e423a7eb1
Alejandro Rodriguez <96800886+alejandrodriguez@users.noreply.github.com>
2024-07-24 13:03:39 -0400

    Added ClearCounter prefab

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,19 +6,28 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private int moveSpeed = 7;
     [SerializeField] private GameInput gameInput;
+    [SerializeField] private LayerMask countersLayerMask;
 
-  private bool isWalking;
+    private bool isWalking;
+    private Vector3 lastInteractDir;
 
-  private void Update()
+    private void Update()
+    {
+        HandleMovement();
+        HandleInteractions();
+    }
+
+    private void HandleMovement()
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         float moveDistance = moveSpeed * Time.deltaTime;
         float playerRadius = 0.7f;
         int playerHeight = 2;
         bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
-        if (!canMove) {
+
+        if (!canMove)
+        {
             // Attempt only X movement
             Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
             canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
@@ -40,12 +49,32 @@ public class Player : MonoBehaviour
 
         }
         if (canMove)
-        { 
+        {
             transform.position += moveDir * moveDistance;
         }
         isWalking = moveDir != Vector3.zero;
         int rotationSpeed = 10;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, rotationSpeed * Time.deltaTime);
+    }
+
+    private void HandleInteractions()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        float interactDistance = 2f;
+
+        if (moveDir != Vector3.zero)
+        {
+            lastInteractDir = moveDir;
+        }
+        if(Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask))
+        {
+            if(raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                // Has ClearCounter
+                clearCounter.Interact();
+            }
+        }
     }
     public bool IsWalking()
     {
